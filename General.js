a005a25c826fa96ff208826dd1220493552f2a82
NiclasVNielsen <57449509+NiclasVNielsen@users.noreply.github.com>
2023-02-07 10:59:55 +0100

    Restructioning

---
+++ General.js
@@ -0,0 +1,12 @@
+/* Must have */
+
+/* Should have */
+const PersonalSettings = {
+    FilterOptions : "Choose if the systems filter should prioritice Dates of Importance"
+}
+const CompanyManagement = {
+    BigCompanyDepartmentGroups : "Make groups inside companies Like GUI team 1, API dev team 4, Deck repair crew 2 etc..."
+}
+
+/* Nice to have */
+const Pirates = "Make everything pirate theme"
--- Project overview page.js
+++ Project overview page.js
@@ -23,14 +23,9 @@ const DisplayProjects = {
     }
 }
 
-/* Should have */
+/* Should have *//* Should have */
 const PersonalSettings = {
-    FilterOptions : "Choose if the systems filter should prioritice Dates of Importance",
     CustomProjectDisplay: "FilterProjects should have a 'Custom setup' option"
-}
-const CompanyManagement = {
-    BigCompanyDepartmentGroups : "Make groups inside companies Like GUI team 1, API dev team 4, Deck repair crew 2 etc..."
 }
 
 /* Nice to have */
-const Pirates = "Make everything pirate theme"
