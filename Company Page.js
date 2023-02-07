c5381a463946c0fff163b4f7577575a7e83b3b07
NiclasVNielsen <Niclasvnielsen@gmail.com>
2023-02-07 15:42:20 +0100

    Create company

--- Company Page.js
+++ Company Page.js
@@ -1,10 +1,8 @@
 /* Must have */
 const CreateCompanyProject = "Create company project button"
-
 const ManageEmployees = "Invite/Remove people to the company"
-
 const ManageGroups = "Create/Update/Delete work groups"
-
+const ManageOwners = "Pick who owns the company"
 const CompanyProjects = {
     /* List of company projects */
     edit : "pop up with editing basic things",
---
+++ Create Company.js
@@ -0,0 +1,3 @@
+/* Must have */
+const Name = "Company Name";
+const Owners = "Defaults to the person who create the company"
